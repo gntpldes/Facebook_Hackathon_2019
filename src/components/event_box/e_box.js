9deca143061b20c537d634722541a958cdafef8f
David Welsh <welshdavmsu@gmail.com>
2019-11-02 15:37:14 -0500

    Final Bits for Input

--- src/components/event_box/e_box.js
+++ src/components/event_box/e_box.js
@@ -1,10 +1,12 @@
 import React from 'react'; 
 import PfPic from './inner_comps/profile_pic'; 
+import UserInfo from './user_comps/user_info';
 
 const Box = () => {
     return(
         <div className="EBox">
             <PfPic />
+            <UserInfo /> 
         </div>
     ); 
 }
---
+++ src/components/event_box/user_comps/user_info.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+
+
+const UserInfo = () => {
+    return(
+        <div className="PDisp">
+            <h1>User Name</h1>
+            <br></br>
+            <hr></hr>
+            <br></br>
+            <ul>
+                <li>Event Name</li>
+                <br></br>
+                <li>Event Location</li>
+                <br></br>
+                <li>Event Time</li>
+            </ul>
+        </div>
+    ); 
+}
+
+export default UserInfo; 
